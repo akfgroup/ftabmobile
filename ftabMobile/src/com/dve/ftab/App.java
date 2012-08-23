c189fdb65c9ac0a1466fb0b271cfed6d63be53f3
David Sauvier <david@sauvier.com>
2012-08-23 17:33:35 -0400

    Initial Commit

---
+++ ftabMobile/src/com/dve/ftab/App.java
@@ -0,0 +1,24 @@
+package com.dve.ftab;
+
+
+import org.apache.cordova.CordovaChromeClient;
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class App extends DroidGap {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+    @Override
+    public void init() {
+    	super.init(new WebView(this), new GWTCordovaWebViewClient(this), new CordovaChromeClient(this));
+    	
+    }
+    
+}
